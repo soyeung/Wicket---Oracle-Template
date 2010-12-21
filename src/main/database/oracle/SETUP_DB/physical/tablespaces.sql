e7d5173a79d4e76a33e00dcf876abf9c135f6388
andrewah <andrewah@hotmail.com>
2010-12-21 19:44:32 +0000

    Fixed tablespaces.sql script

--- src/main/database/oracle/SETUP_DB/physical/tablespaces.sql
+++ src/main/database/oracle/SETUP_DB/physical/tablespaces.sql
@@ -2,15 +2,15 @@ define default_tablespace       = TS_TEMPLATE
 define default_temp_tablespace  = TTS_TEMPLATE
 define default_undo_tablespace  = UTS_TEMPLATE
 
-define path_to_default_tablespace       = 'c:/oracle_tablespace/ts_template.dbf'
-define path_to_default_temp_tablespace  = 'c:/oracle_tablespace/tts_template.dbf'
-define path_to_default_undo_tablespace  = 'c:/oracle_tablespace/uts_template.dbf'
+define path_to_dft_tablespace       = 'c:/oracle_tablespace/ts_template.dbf'
+define path_to_dft_temp_tablespace  = 'c:/oracle_tablespace/tts_template.dbf'
+define path_to_dft_undo_tablespace  = 'c:/oracle_tablespace/uts_template.dbf'
 
 drop tablespace &default_tablespace including contents and datafiles cascade constraints
 /
 
 create smallfile tablespace &default_tablespace
-datafile '&path_to_default_tablespace'
+datafile '&path_to_dft_tablespace'
 size 50m autoextend on
 logging
 extent management local
@@ -22,13 +22,13 @@ drop tablespace &default_temp_tablespace including contents and datafiles cascad
 /
 
 create smallfile temporary tablespace &default_temp_tablespace
-tempfile '&path_to_default_temp_tablespace'
+tempfile '&path_to_dft_temp_tablespace'
 size 50m autoextend on
 extent management local
 /
 
 create undo tablespace &default_undo_tablespace
-datafile '&path_to_default_undo_tablespace'
+datafile '&path_to_dft_undo_tablespace'
 size 50m reuse autoextend on 
 extent management local
 /
