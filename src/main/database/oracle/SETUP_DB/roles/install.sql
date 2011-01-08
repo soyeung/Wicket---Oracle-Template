0612526eb3a837f249245f6937d556fb7d68b17d
andrewah <andrewah@hotmail.com>
2011-01-08 11:22:06 +0000

    changed setup_db to SETUP_DB as paths are case sensitive in linux

--- src/main/database/oracle/SETUP_DB/roles/install.sql
+++ src/main/database/oracle/SETUP_DB/roles/install.sql
@@ -1,65 +1,65 @@
-@&path_to_this_file/setup_db/roles/role_create_session
+@&path_to_this_file/SETUP_DB/roles/role_create_session
 
-@&path_to_this_file/setup_db/roles/role_delegate_app_user_mgr
+@&path_to_this_file/SETUP_DB/roles/role_delegate_app_user_mgr
 
-@&path_to_this_file/setup_db/roles/role_dgt_app_user_role_mgr
+@&path_to_this_file/SETUP_DB/roles/role_dgt_app_user_role_mgr
 
-@&path_to_this_file/setup_db/roles/role_standard_app_user_creator
+@&path_to_this_file/SETUP_DB/roles/role_standard_app_user_creator
 
-@&path_to_this_file/setup_db/roles/role_standard_app_user_mgr
+@&path_to_this_file/SETUP_DB/roles/role_standard_app_user_mgr
 
-@&path_to_this_file/setup_db/roles/role_std_app_user_role_mgr
+@&path_to_this_file/SETUP_DB/roles/role_std_app_user_role_mgr
 
-@&path_to_this_file/setup_db/roles/role_std_user_password_mgr
+@&path_to_this_file/SETUP_DB/roles/role_std_user_password_mgr
 
-@&path_to_this_file/setup_db/roles/role_std_app_user_role_mgr
+@&path_to_this_file/SETUP_DB/roles/role_std_app_user_role_mgr
 
-@&path_to_this_file/setup_db/roles/role_configure_ref_data
+@&path_to_this_file/SETUP_DB/roles/role_configure_ref_data
 
-@&path_to_this_file/setup_db/roles/role_ref_data_mgr
+@&path_to_this_file/SETUP_DB/roles/role_ref_data_mgr
 
-@&path_to_this_file/setup_db/roles/role_rpt_authenticate
+@&path_to_this_file/SETUP_DB/roles/role_rpt_authenticate
 
-@&path_to_this_file/setup_db/roles/role_report_user
+@&path_to_this_file/SETUP_DB/roles/role_report_user
 
-@&path_to_this_file/setup_db/roles/role_security_matrix_by_user
+@&path_to_this_file/SETUP_DB/roles/role_security_matrix_by_user
 
-@&path_to_this_file/setup_db/roles/role_security_matrix_by_role
+@&path_to_this_file/SETUP_DB/roles/role_security_matrix_by_role
 
-@&path_to_this_file/setup_db/roles/role_unindexed_fk_report
+@&path_to_this_file/SETUP_DB/roles/role_unindexed_fk_report
 
-@&path_to_this_file/setup_db/roles/role_ucp_mgr
+@&path_to_this_file/SETUP_DB/roles/role_ucp_mgr
 
-@&path_to_this_file/setup_db/roles/role_rdm_app_user_profile
+@&path_to_this_file/SETUP_DB/roles/role_rdm_app_user_profile
 
-@&path_to_this_file/setup_db/roles/role_rdm_app_user_type
+@&path_to_this_file/SETUP_DB/roles/role_rdm_app_user_type
 
-@&path_to_this_file/setup_db/roles/role_rdm_app_user_type_role
+@&path_to_this_file/SETUP_DB/roles/role_rdm_app_user_type_role
 
-@&path_to_this_file/setup_db/roles/role_rdm_db_role
+@&path_to_this_file/SETUP_DB/roles/role_rdm_db_role
 
-@&path_to_this_file/setup_db/roles/role_rdm_language
+@&path_to_this_file/SETUP_DB/roles/role_rdm_language
 
-@&path_to_this_file/setup_db/roles/role_rdm_manageable_db_role
+@&path_to_this_file/SETUP_DB/roles/role_rdm_manageable_db_role
 
-@&path_to_this_file/setup_db/roles/role_rdm_report
+@&path_to_this_file/SETUP_DB/roles/role_rdm_report
 
-@&path_to_this_file/setup_db/roles/role_rdm_report_category
+@&path_to_this_file/SETUP_DB/roles/role_rdm_report_category
 
-@&path_to_this_file/setup_db/roles/role_rdm_report_cat_report
+@&path_to_this_file/SETUP_DB/roles/role_rdm_report_cat_report
 
-@&path_to_this_file/setup_db/roles/role_curr_user_activity_report
+@&path_to_this_file/SETUP_DB/roles/role_curr_user_activity_report
 
-@&path_to_this_file/setup_db/roles/role_tables_without_primary_keys_report
+@&path_to_this_file/SETUP_DB/roles/role_tables_without_primary_keys_report
 
-@&path_to_this_file/setup_db/roles/role_session_lock_report
+@&path_to_this_file/SETUP_DB/roles/role_session_lock_report
 
-@&path_to_this_file/setup_db/roles/role_view_debug_info
+@&path_to_this_file/SETUP_DB/roles/role_view_debug_info
 
-@&path_to_this_file/setup_db/roles/role_usage_data_report
+@&path_to_this_file/SETUP_DB/roles/role_usage_data_report
 
-@&path_to_this_file/setup_db/roles/role_security_model_report
+@&path_to_this_file/SETUP_DB/roles/role_security_model_report
 
-@&path_to_this_file/setup_db/roles/role_user_privileges_report
+@&path_to_this_file/SETUP_DB/roles/role_user_privileges_report
 
-@&path_to_this_file/setup_db/roles/role_least_privileges_report
+@&path_to_this_file/SETUP_DB/roles/role_least_privileges_report
